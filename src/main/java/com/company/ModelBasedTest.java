92ca759708371dda0f4ee2f0fc3463c5e46192ff
Alex Karamfilov <karamfilovs@gmail.com>
2018-11-21 16:02:59 +0200

    Renamed some of the artifacts

--- src/main/java/com/company/ModelBasedTest.java
+++ src/main/java/com/company/ModelBasedTest.java
@@ -1,14 +1,14 @@
-  package com.company;
+package com.company;
 
-  import core.Inv;
-  import org.graphwalker.core.machine.ExecutionContext;
-  import org.graphwalker.java.annotation.GraphWalker;
+import core.Inv;
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.java.annotation.GraphWalker;
 
 /**
  * See: http://graphwalker.org/docs/maven_archetype for more details
  * <p/>
- * Implements the GraphWalker model: src/main/resources/SmallTest.graphml
- * The SmallTest.graphml can be opened and edited using http://www.yworks.com/en/products/yfiles/yed/
+ * Implements the GraphWalker model: src/main/resources/InvModel.graphml
+ * The InvModel.graphml can be opened and edited using http://www.yworks.com/en/products/yfiles/yed/
  * <p/>
  * For convienicene, a jpg image exists side-by-side in the same folder.
  * <p/>
@@ -23,50 +23,47 @@
  * also: mvn graphwalker:test
  */
 @GraphWalker(value = "random(edge_coverage(1000))", start = "e_GoToLoginPage")
-public class ModelBasedTest extends ExecutionContext implements SmallTest {
+public class ModelBasedTest extends ExecutionContext implements InvModel {
     private Inv inv;
 
-    public ModelBasedTest(){
+    public ModelBasedTest() {
         inv = new Inv();
         inv.startBrowser("chrome");
     }
 
 
-  @Override
-  public void v_VerifyAtLoginPage() {
-      inv.loginPage().verifyCompanyName("QA Ground");
+    @Override
+    public void v_VerifyAtLoginPage() {
+        inv.loginPage().verifyCompanyName("QA Ground");
 
 
-  }
+    }
 
-  @Override
-  public void v_VerifyAtHomePage() {
-      inv.homePage().verifyLoggedUser("karamfilovs@gmail.com");
+    @Override
+    public void v_VerifyAtHomePage() {
+        inv.homePage().verifyLoggedUser("karamfilovs@gmail.com");
+    }
 
-  }
+    @Override
+    public void e_ClickHomePageIcon() {
+        inv.homePage().clickCompanyLogo();
 
-  @Override
-  public void e_ClickHomePageIcon() {
-      inv.homePage().clickCompanyLogo();
+    }
 
-  }
+    @Override
+    public void e_Logout() {
+        inv.homePage().clickLogoutLink();
+    }
 
-  @Override
-  public void e_Logout() {
-      inv.homePage().clickLogoutLink();
+    @Override
+    public void e_Login() {
+        inv.loginPage().login("karamfilovs@gmail.com", "123456");
+    }
 
-  }
-
-  @Override
-  public void e_Login() {
-      inv.loginPage().login("karamfilovs@gmail.com", "123456");
-
-  }
-
-  @Override
-  public void e_GoToLoginPage() {
-      inv.loginPage().gotoPage();
-  }
+    @Override
+    public void e_GoToLoginPage() {
+        inv.loginPage().gotoPage();
+    }
 
     @Override
     public void e_ClickItemLink() {
